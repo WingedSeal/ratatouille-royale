05500ed8f6da3ac84db76186780ac4259ab63bfd
WingedSeal <firm09719@gmail.com>
2025-08-20 15:41:24 +0700

    feat: remove tile modifier

--- src/ratroyale/tile.py
+++ src/ratroyale/tile.py
@@ -5,13 +5,8 @@ from .hexagon import OddRCoord
 from .entity import Entity
 
 
-class TileModifier(Enum):
-    ON_FIRE = auto()
-
-
 @dataclass
 class Tile:
     coord: OddRCoord
     entities: list[Entity]
-    modifiers: list[TileModifier]
     height: int
