df6e00275eed3f70e951385b9161a162ba08fe4b
WingedSeal <firm09719@gmail.com>
2025-08-20 15:45:54 +0700

    feat: add feature

--- src/ratroyale/map.py
+++ src/ratroyale/map.py
@@ -1,4 +1,6 @@
+from .features.feature import Feature
 from .tile import Tile
+from .hexagon import OddRCoord
 from pathlib import Path
 
 MAP_FILE_EXTENSION = "rrmap"
@@ -7,6 +9,7 @@ MAP_FILE_EXTENSION = "rrmap"
 class Map:
     size_x: int
     size_y: int
+    features: list[tuple[Feature, OddRCoord]]
     tiles: list[list[Tile]]
 
     def __init__(self, size_x: int, size_y: int, tiles: list[list[Tile]]) -> None:
