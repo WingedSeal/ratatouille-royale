23745a69eec4e6262f3fc0bc9b7d0b15d098ad77
WingedSeal <firm09719@gmail.com>
2025-08-20 14:24:45 +0700

    feat: implement basic entity and rodent

--- src/ratroyale/entities/rodent.py
+++ src/ratroyale/entities/rodent.py
@@ -1,5 +1,86 @@
+from abc import ABCMeta, abstractmethod
+import inspect
+from dataclasses import asdict, dataclass
 from ..entity import Entity
+from ..hexagon import OddRCoord
+from typing import Any, Callable, TypeVar, cast
 
 
-class Rodent(Entity):
-    pass
+class RodentMeta(ABCMeta):
+    def __new__(cls, name: str, bases: tuple[type, ...], dct: dict[str, Any]):
+        if bases and not dct.get("_has_rodent_data", False):
+            raise TypeError(
+                f"'{name}' must be decorated with @rodent_data(...)")
+        return super().__new__(cls, name, bases, dct)
+
+
+@dataclass
+class _RodentSkill:
+    method_name: str
+    reach: int
+    crumb_cost: int
+
+
+@dataclass
+class RodentSkill(_RodentSkill):
+    func: Callable[[], None]
+
+
+class Rodent(Entity, metaclass=RodentMeta):
+    _has_rodent_data = False
+    skills: list[RodentSkill] = []
+    speed: int
+    stamina: int
+    move_cost: int
+    attack: int
+    height: int
+
+    def __init__(self, pos: OddRCoord) -> None:
+        super().__init__(pos)
+
+    @abstractmethod
+    def skill_descriptions(self) -> list[str]:
+        ...
+
+
+T = TypeVar('T', bound=Rodent)
+
+
+def rodent_data(speed: int,
+                stamina: int,
+                move_cost: int,
+                attack: int,
+                height: int,
+                health: int | None = None,
+                defense: int | None = None,
+                description: str = "",
+                skills: list[_RodentSkill] = [],
+                ):
+    def wrapper(cls: type[T]) -> type[T]:
+        assert issubclass(cls, Rodent)
+        cls._has_rodent_data = True
+        cls.health = health
+        cls.defense = defense
+        cls.description = description
+        cls.speed = speed
+        cls.stamina = stamina
+        cls.move_cost = move_cost
+        cls.attack = attack
+        cls.height = height
+        for skill in skills:
+            if not hasattr(cls, skill.method_name):
+                raise ValueError(
+                    f"{skill} is not an attribute of {cls.__name__}")
+            skill_function = getattr(cls, skill.method_name)
+            if not callable(skill_function):
+                raise ValueError(
+                    f"{skill} is not callable")
+            arg_count = len(inspect.signature(skill_function).parameters)
+            if arg_count != 0:
+                raise ValueError(
+                    f"Expected {skill} method to take 0 arguments (got {arg_count})"
+                )
+            cls.skills.append(RodentSkill(
+                **asdict(skill), func=cast(Callable[[], None], skill_function)))
+        return cls
+    return wrapper
--- src/ratroyale/entity.py
+++ src/ratroyale/entity.py
@@ -1,4 +1,5 @@
 from .hexagon import OddRCoord
+from typing import TypeVar
 
 
 class Entity:
@@ -6,6 +7,45 @@ class Entity:
     Any entity on the tile system.
     """
     pos: OddRCoord
+    health: int | None = None
+    defense: int | None = None
+    description: str = ""
 
     def __init__(self, pos: OddRCoord) -> None:
         self.pos = pos
+
+    def on_damage_taken(self, damage: int) -> int | None:
+        pass
+
+    def on_death(self) -> bool:
+        return True
+
+    def take_damage(self, damage: int) -> bool:
+        """
+        Take damage and reduce health accordingly if entity has health
+        :param damage: How much damage taken
+        :returns: Whether the entity die
+        """
+        new_damage = self.on_damage_taken(damage)
+        if new_damage is not None:
+            damage = new_damage
+        if self.health is None:
+            return False
+        self.health -= max(0, damage - (self.defense or 0))
+        if self.health <= 0:
+            self.health = 0
+            return True
+        return False
+
+
+T = TypeVar('T', bound=Entity)
+
+
+def entity_data(health: int | None = None, defense: int | None = None, description: str = ""):
+    def wrapper(cls: type[T]) -> type[T]:
+        assert issubclass(cls, Entity)
+        cls.health = health
+        cls.defense = defense
+        cls.description = description
+        return cls
+    return wrapper
