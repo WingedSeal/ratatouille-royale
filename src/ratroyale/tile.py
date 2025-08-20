f4ad2e99d8ae07e89b5727e5fa10ca3c88ea387c
WingedSeal <firm09719@gmail.com>
2025-08-20 15:21:12 +0700

    feat: add Tile

--- src/ratroyale/tile.py
+++ src/ratroyale/tile.py
@@ -1,2 +1,17 @@
+from dataclasses import dataclass
+from enum import Enum, auto
+
+from .hexagon import OddRCoord
+from .entity import Entity
+
+
+class TileModifier(Enum):
+    ON_FIRE = auto()
+
+
+@dataclass
 class Tile:
-    pass
+    coord: OddRCoord
+    entities: list[Entity]
+    modifiers: list[TileModifier]
+    height: int
