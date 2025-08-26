e844552d5466de2b09a39b7ca153c5bee31f6408
WingedSeal <firm09719@gmail.com>
2025-08-26 21:39:34 +0700

    feat: add explosion damage

--- src/ratroyale/backend/entities/rodents/common_skills.py
+++ src/ratroyale/backend/entities/rodents/common_skills.py
@@ -1,10 +1,12 @@
 from typing import TYPE_CHECKING
 
+
 from ...skill_callback import SkillCallback, skill_callback_check
 
 
 if TYPE_CHECKING:
     from ...game_manager import GameManager
+    from ...tile import Tile
     from ...entities.rodent import Rodent
     from ...entity import EntitySkill, SkillResult
     from ...board import Board
@@ -33,4 +35,29 @@ def normal_damage(damage: int) -> SkillCallback:
         for target in selected_targets:
             game_manager.board.damage_entity(
                 game_manager.get_enemy_on_pos(target), damage)
+    return callback
+
+
+def explosion_damage(damage: int, radius: int, *, is_stackable: bool = False) -> SkillCallback:
+    @skill_callback_check
+    def callback(game_manager: "GameManager", selected_targets: list["OddRCoord"]) -> None:
+        tagged_coord: set["OddRCoord"] = set()
+        for selected_target in selected_targets:
+            selected_tile = game_manager.board.get_tile(selected_target)
+            if selected_tile is None:
+                raise ValueError("Invalid Tile was selected")
+            selected_tile_ = selected_tile
+
+            def __is_coord_blocked(target_coord: "OddRCoord", source_coord: "OddRCoord") -> bool:
+                tile = game_manager.board.get_tile(target_coord)
+                if tile is None:
+                    return True
+                return selected_tile_.height + radius < tile.get_total_height(game_manager.turn)
+            for coord in selected_target.get_reachable_coords(radius, __is_coord_blocked, is_include_self=True):
+                if not is_stackable and coord in tagged_coord:
+                    continue
+                game_manager.board.damage_entity(
+                    game_manager.get_enemy_on_pos(coord), damage)
+                if not is_stackable:
+                    tagged_coord.add(coord)
     return callback
--- src/ratroyale/backend/game_manager.py
+++ src/ratroyale/backend/game_manager.py
@@ -1,6 +1,7 @@
 import math
 from queue import Queue
 from random import shuffle
+from typing import Iterator
 
 from .game_event import GameEvent
 from .error import NotEnoughCrumbError
@@ -82,6 +83,20 @@ class GameManager:
                 continue
             return entity
         raise ValueError(f"No valid target on this pos ({pos})")
+
+    def get_enemies_on_pos(self, pos: OddRCoord) -> Iterator[Entity]:
+        """
+        Get every damagable enemies at position. Raise error if there's nothing there
+        """
+        tile = self.board.get_tile(pos)
+        if tile is None:
+            raise ValueError("There is no tile on the coord")
+        for entity in reversed(tile.entities):
+            if entity.health is None:
+                continue
+            if entity.side == self.turn:
+                continue
+            yield entity
 
     def draw_squeak(self, side: Side) -> Squeak:
         """
