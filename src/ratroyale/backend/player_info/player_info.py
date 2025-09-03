0af89385a83c50de1afe4d2c590ca534132d4640
WingedSeal <firm09719@gmail.com>
2025-09-03 15:36:05 +0700

    feat: update PlayerInfo

--- src/ratroyale/backend/player_info/player_info.py
+++ src/ratroyale/backend/player_info/player_info.py
@@ -1,16 +1,20 @@
 from pathlib import Path
+
+from .squeak import Squeak
 from .squeak_set import SqueakSet
 
 SAVE_FILE_EXTENSION = "save"
 
 
 class PlayerInfo:
+    all_squeaks: list[Squeak]
     squeak_set: SqueakSet
 
-    def __init__(self, squeak_set: SqueakSet) -> None:
+    def __init__(self, all_squeaks: list[Squeak], squeak_set: SqueakSet) -> None:
+        self.all_squeaks = all_squeaks
         self.squeak_set = squeak_set
 
     @classmethod
     def from_file(cls, file: Path) -> "PlayerInfo":
         # TODO
-        return cls(SqueakSet())
+        return cls([], SqueakSet(deck=[]))
--- src/ratroyale/backend/player_info/squeak_set.py
+++ src/ratroyale/backend/player_info/squeak_set.py
@@ -3,3 +3,6 @@ from .squeak import Squeak
 
 class SqueakSet:
     deck: list[Squeak]
+
+    def __init__(self, deck: list[Squeak]) -> None:
+        self.deck = deck
