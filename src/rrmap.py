211e4fe942955092a7fe683c09f4b65126081e22
WingedSeal <firm09719@gmail.com>
2025-10-01 15:28:03 +0700

    feat: add cli for tiled_to_rrmap and gen_tileset

---
+++ src/rrmap.py
@@ -0,0 +1,44 @@
+from json import load
+from pathlib import Path
+from ratroyale.backend import tiled_tmj
+import sys
+
+
+def tiled_to_rrmap(map_name: str) -> None:
+    with Path("rrmap.tmx").open("r") as f:
+        map = tiled_tmj.tmj_to_map(load(f), map_name)
+    map.to_file(Path(f"{map_name.lower().replace(' ', '-')}.rrmap"))
+
+
+def gen_tileset(size: str) -> None:
+    if "x" not in size:
+        print("Size must be in format ROWxCOL, e.g., 10x10")
+        sys.exit(1)
+
+    row, col = map(int, size.lower().split("x"))
+    old_tileset_image = sys.argv[2] if len(sys.argv) > 2 else None
+    if old_tileset_image is None:
+        tiled_tmj.gen_tileset_tsx(row, col)
+    else:
+        tiled_tmj.gen_tileset_tsx(row, col, old_tileset_image)
+
+
+def main() -> None:
+    cmd, *args = sys.argv[1:]
+    if cmd == "tiled_to_rrmap":
+        if len(args) != 1:
+            print("Usage: poe tiled_to_rrmap <map_name>")
+            sys.exit(1)
+        tiled_to_rrmap(args[0])
+    elif cmd == "gen_tileset":
+        if len(args) not in (1, 2):
+            print("Usage: poe gen_tileset <row>x<col> [old_tileset_image]")
+            sys.exit(1)
+        gen_tileset(*args)
+    else:
+        print(f"Unknown command: {cmd}")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
