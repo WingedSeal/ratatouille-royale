8a0c0a7181b51c7d1e3c290aec9ec61d5ab9fdd8
WingedSeal <firm09719@gmail.com>
2025-10-07 19:23:31 +0700

    feat: add pytest

---
+++ tests/backend/test_side.py
@@ -0,0 +1,12 @@
+import pytest
+from src.ratroyale.backend.side import Side
+
+
+@pytest.mark.parametrize("value", range(len(Side) + 1))
+def test_side_int(value: int) -> None:
+    assert Side.to_int(Side.from_int(value)) == value
+
+
+def test_other_side() -> None:
+    assert Side.MOUSE.other_side() == Side.RAT
+    assert Side.RAT.other_side() == Side.MOUSE
