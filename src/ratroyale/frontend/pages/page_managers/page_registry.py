642b9fcc7c278bdd2d0ca917f0935e15c0e19e73
Nattapol1994 <6522781994@g.siit.tu.ac.th>
2025-10-08 18:59:36 +0700

    minor: page_registry path config

--- src/ratroyale/frontend/pages/page_managers/page_registry.py
+++ src/ratroyale/frontend/pages/page_managers/page_registry.py
@@ -5,10 +5,16 @@ import sys
 from pathlib import Path
 import importlib
 
+# region Path Configuration
+
 frontend_folder = Path(__file__).parent.parent
 sys.path.append(str(frontend_folder.resolve()))
 
 PAGES_FOLDER = frontend_folder / "page_definitions"
+MODULE_PATH = "ratroyale.frontend.pages.page_definitions."
+
+# endregion
+
 _PAGE_REGISTRY: dict[str, type[Page]] = {}
 
 
@@ -17,7 +23,7 @@ def auto_import_pages() -> None:
         module_name = py_file.stem
         if module_name == "__init__":
             continue
-        module_path = f"ratroyale.frontend.pages.page_definitions.{module_name}"
+        module_path = MODULE_PATH + module_name
         try:
             importlib.import_module(module_path)
         except Exception as e:
