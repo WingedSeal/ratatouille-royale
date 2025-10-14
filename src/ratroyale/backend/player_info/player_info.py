486cdd80d4638f67c6fba88ae9920eee46757448
WingedSeal <firm09719@gmail.com>
2025-10-14 14:31:52 +0700

    feat: add PlayerInfo.save

--- src/ratroyale/backend/player_info/player_info.py
+++ src/ratroyale/backend/player_info/player_info.py
@@ -7,7 +7,7 @@ from .squeak import Squeak
 from .squeak_set import SqueakSet
 from . import squeaks  # noqa
 
-
+HAND_LENGTH = 5
 SAVE_FILE_EXTENSION = "save"
 ENDIAN: Final = "big"
 
@@ -19,7 +19,7 @@ class PlayerInfo:
 
     _FORMAT_SPEC = """
     2 bytes for all_squeaks_length
-    large_all_squeaks_length = all_squeaks_length > 255
+    large_all_squeaks_length = 1 if all_squeaks_length > 255 else 0
     loop all_squeaks_length times {
         1 byte for squeak_name_length
         squeak_name_length bytes for squeak_name
@@ -100,6 +100,28 @@ class PlayerInfo:
 
     def save(self) -> bytes:
         data = bytearray()
+        all_squeaks_length = len(self.all_squeaks)
+        data.extend(all_squeaks_length.to_bytes(2, ENDIAN))
+        large_all_squeaks_length = 1 if all_squeaks_length > 255 else 0
+        for squeak in self.all_squeaks:
+            encoded_squeak_name = squeak.name.encode()
+            data.append(len(encoded_squeak_name))
+            data.extend(encoded_squeak_name)
+
+        squeak_sets_count = len(self.squeak_sets)
+        data.append(squeak_sets_count)
+        for squeak_set in self.squeak_sets:
+            squeak_set_length = len(squeak_set.deck_index)
+            data.append(squeak_set_length)
+            for squeak_index in squeak_set.deck_index:
+                data.extend(squeak_index.to_bytes(1 + large_all_squeaks_length, ENDIAN))
+        for squeak_set in self.squeak_sets:
+            assert len(squeak_set.hands_index) == HAND_LENGTH
+            for squeak_index in squeak_set.hands_index:
+                data.extend(squeak_index.to_bytes(1 + large_all_squeaks_length, ENDIAN))
+
+        data.append(self.selected_squeak_set_index)
+
         return bytes(data)
 
     @classmethod
