e02288b45567a1c0529a6367e94ef976271963e1
Peepo <puwarit420@gmail.com>
2025-10-15 15:03:04 +0700

    feat: add cancel skill panel

--- src/ratroyale/frontend/pages/page_definitions/select_target_prompt.py
+++ src/ratroyale/frontend/pages/page_definitions/select_target_prompt.py
@@ -27,57 +27,57 @@ class SelectTargetPromptPage(Page):
         gui_elements: list[UIRegisterForm] = []
 
         # region Panel + nested button
-        panel_element = pygame_gui.elements.UIPanel(
-            relative_rect=pygame.Rect(0, 0, 400, 300),
+        select_target_panel = pygame_gui.elements.UIPanel(
+            relative_rect=pygame.Rect(200, 100, 400, 100),
             manager=self.gui_manager,
             object_id=pygame_gui.core.ObjectID(
-                class_id="AbilityPanel", object_id="panel_event"
+                class_id="SelectTargetPanel", object_id="select_target_panel"
             ),
         )
-        gui_elements.append(UIRegisterForm("test_panel", panel_element))
+        gui_elements.append(UIRegisterForm("test_panel", select_target_panel))
 
         # Nested button inside the panel
         pygame_gui.elements.UIButton(
-            relative_rect=pygame.Rect(0, 30, 150, 30),
-            text="test btn",
+            relative_rect=pygame.Rect(300, 10, 90, 70),
+            text="Cancel Skill",
             manager=self.gui_manager,
-            container=panel_element,
+            container=select_target_panel,
             object_id=pygame_gui.core.ObjectID(
-                class_id="AbilityButton", object_id="close_button"
+                class_id="CancelSkillButton", object_id="cancel_skill_button"
             ),
         )
         # endregion
 
         # region Other buttons
-        gui_elements.append(
-            UIRegisterForm(
-                "registered_name",  # <- registered name. for getting & deleting.
-                pygame_gui.elements.UIButton(
-                    relative_rect=pygame.Rect(200, 100, 200, 50),
-                    text="Test Button",
-                    manager=self.gui_manager,
-                    object_id=pygame_gui.core.ObjectID(
-                        class_id="whatever",  # <- theming name.
-                        object_id="event_name",  # <- event name. used for listening.
-                    ),
-                ),
-            )
-        )
+        # gui_elements.append(
+        #     UIRegisterForm(
+        #         "registered_name",  # <- registered name. for getting & deleting.
+        #         pygame_gui.elements.UIButton(
+        #             relative_rect=pygame.Rect(200, 100, 200, 50),
+        #             text="Test Button",
+        #             manager=self.gui_manager,
+        #             object_id=pygame_gui.core.ObjectID(
+        #                 class_id="whatever",  # <- theming name.
+        #                 object_id="event_name",  # <- event name. used for listening.
+        #             ),
+        #         ),
+        #     )
+        # )
 
-        gui_elements.append(
-            UIRegisterForm(
-                "registered_name_2",  # <- registered name. for getting & deleting.
-                pygame_gui.elements.UIButton(
-                    relative_rect=pygame.Rect(400, 100, 200, 50),
-                    text="Test Button",
-                    manager=self.gui_manager,
-                    object_id=pygame_gui.core.ObjectID(
-                        class_id="whatever",  # <- theming name.
-                        object_id="event_name_2",  # <- event name. used for listening.
-                    ),
-                ),
-            )
-        )
+        # gui_elements.append(
+        #     UIRegisterForm(
+        #         "registered_name_2",  # <- registered name. for getting & deleting.
+        #         pygame_gui.elements.UIButton(
+        #             relative_rect=pygame.Rect(400, 100, 200, 50),
+        #             text="Test Button",
+        #             manager=self.gui_manager,
+        #             object_id=pygame_gui.core.ObjectID(
+        #                 class_id="whatever",  # <- theming name.
+        #                 object_id="event_name_2",  # <- event name. used for listening.
+        #             ),
+        #         ),
+        #     )
+        # )
         # endregion
 
         # All elements returned here are automatically registered at the start of page.
@@ -103,9 +103,16 @@ class SelectTargetPromptPage(Page):
             )
         )
 
-    @input_event_bind("close_button", pygame_gui.UI_BUTTON_PRESSED)
+    @input_event_bind("cancel_skill_button", pygame_gui.UI_BUTTON_PRESSED)
     def close_panel(self, msg: pygame.event.Event) -> None:
-        self._element_manager.remove_gui_element("test_panel")
+        self.coordination_manager.put_message(
+            PageNavigationEvent(
+                action_list=[
+                    (PageNavigation.CLOSE, "SelectTargetPromptPage"),
+                ]
+            )
+        )
+        # self._element_manager.remove_gui_element("test_panel")
 
     @input_event_bind("event_name_2", pygame_gui.UI_BUTTON_PRESSED)
     def communicate(self, msg: pygame.event.Event) -> None:
