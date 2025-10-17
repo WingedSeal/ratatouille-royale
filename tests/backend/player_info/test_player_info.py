58698382c0164eac1a64fc6544ae11d030667a4b
WingedSeal <firm09719@gmail.com>
2025-10-17 18:01:02 +0700

    feat(player_info): test protect and unprotect

--- tests/backend/player_info/test_player_info.py
+++ tests/backend/player_info/test_player_info.py
@@ -1,6 +1,10 @@
-from ratroyale.backend.player_info.player_info import PlayerInfo
+from ratroyale.backend.player_info.player_info import PlayerInfo, protect, unprotect
 
 
 def test_player_info_test_load(example_player_info: PlayerInfo) -> None:
     player_info_bytes = example_player_info.save()
     assert player_info_bytes == PlayerInfo.load(player_info_bytes).save()
+
+    assert player_info_bytes == unprotect(
+        protect(PlayerInfo.load(player_info_bytes).save())
+    )
